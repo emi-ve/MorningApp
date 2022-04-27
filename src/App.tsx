0c6c075263f2b057fa337823620813871b18591e
Emily V <emily.venturino@outlook.com>
2022-04-26 20:58:59 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import * as React from 'react';
+import { Link} from 'react-router-dom';
+import Main from './Main';
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>  
+    
+      <div className="topNav">
+        <ul>
+          <li><Link to='/' className="link">Home</Link></li>
+          <li><Link to='/topics' className="link">Topics</Link></li>
+          <li><Link to='/settings' className="link">Settings</Link></li>
+        </ul>
+        <hr />
+             
+      </div> 
+      <Main />  
+     
+    </>
+  )
 }
-
-export default App;
