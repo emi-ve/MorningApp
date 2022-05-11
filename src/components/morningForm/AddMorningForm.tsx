59e2755cc34a8f15eb022274c6b6b4c74bb8ce85
Emily Venturino <ebventurino@gmail.com>
2022-05-10 22:17:30 -0500

    rendering to dom

--- src/components/morningForm/AddMorningForm.tsx
+++ src/components/morningForm/AddMorningForm.tsx
@@ -3,9 +3,13 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 
+type CounterState = {
+    response: string;
+};
+
 export const AddMorningForm: React.FC = () => {
 
-  
+
 
     const [term, setTerm] = useState('');
     const submitForm = (event: React.FormEvent<HTMLFormElement>) => {
@@ -13,26 +17,36 @@ export const AddMorningForm: React.FC = () => {
         event.preventDefault();
         // Do something 
         console.log(term);
-       
-        
+        console.log(setTerm);
+
+        const show = document.getElementById('app');
+        if (show) {
+            // inside of this if branch, show can only be `HTMLElement`
+            show.innerHTML = term;
+        } else {
+            console.error("no element found with id 'row'");
+        }
     }
 
+
+    let form = document.getElementById('app')
     return (
         <div className="taskListForm">
             <div className="w-25 ml-0 mx-auto text-center">
                 <Form className="mb-3 px-30" onSubmit={submitForm}>
                     <Form.Group className="mb-3 px-30" controlId="formBasicEmail">
                         <Form.Label>What are you grateful for today? </Form.Label>
-                        <Form.Control type="text" placeholder="I am grateful today because..." value={term}
+                        <Form.Control type="text" placeholder="I am grateful today because..."
                             onChange={(e) => setTerm(e.target.value)} />
                     </Form.Group>
 
-                    <Button variant="primary" type="submit">
+                    <Button type="submit">
                         Submit
                     </Button>
                 </Form>
             </div>
-            <div id="app">{term}</div>
+            <div id="app"></div>
+
 
         </div>
     );
